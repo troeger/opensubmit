f65d022e85afe145536ed7033be99ba666e320db
troeger <peter@troeger.eu>
2012-11-07 23:41:26 +0100

    job script fixes

--- jobexec/executor.py
+++ jobexec/executor.py
@@ -1,11 +1,12 @@
-import urllib, urllib2, logging, zipfile
+import urllib, urllib2, logging, zipfile, tarfile, tempfile, os, shutil
 from datetime import datetime
 
 # BEGIN Configuration
 logging.basicConfig(level=logging.DEBUG)
 submit_server = "http://localhost:8000"
 secret = "39845zut93purfh977TTTiuhgalkjfnk89"		
-target_dir = "/tmp/"
+#targetdir=tempfile.mkdtemp()+"/"
+targetdir="/tmp/jobexec/"
 # END Configuration
 
 def send_result(msg, error_code, submission_file_id):
@@ -20,7 +21,7 @@ def send_result(msg, error_code, submission_file_id):
 def fetch_job():
 	try:
 		result = urllib2.urlopen("%s/jobs/secret=%s"%(submit_server,secret))
-		fname=target_dir+datetime.now().isoformat()
+		fname=targetdir+datetime.now().isoformat()
 		submid=result.info()['SubmissionFileId']
 		logging.info("Retrieved submission file %s: %s"%(submid, fname))
 		target=open(fname,"wb")
@@ -34,12 +35,19 @@ def fetch_job():
 		else:
 			logging.error(str(e))
 			exit(-1)
-def unpack_job(fname, submid):
-	logging.info("Unpacking "+fname)
-	if not zipfile.is_zipfile(fname):
-		send_result("This is not a valid ZIP file.",-1, submid)
-		exit(-1)
-	logging.info("Valid ZIP file")
+
+def unpack_run_job(fname, submid, targetdir):
+	# os.chroot is not working with tarfile support
+	if zipfile.is_zipfile(fname):
+		logging.info("Valid ZIP file")
+	if tarfile.is_tarfile(fname):
+		logging.info("Valid TAR file")
+		tar = tarfile.open(fname)
+		tar.extractall(targetdir)
+		tar.close()
+	logging.info("Invalid compressed file")
+#	send_result("This is not a valid compressed file.",-1, submid)
+	exit(-1)
 
 fname, submid=fetch_job()
-testdir=unpack_job(fname, submid)
+unpack_run_job(fname, submid, targetdir)
