4882851738bb36ddb356d4723272daac91ee1e5e
Peter Tröger <peter@troeger.eu>
2014-10-24 17:44:52 +0200

    Running full executor tests

--- opensubmit/tests/cases.py
+++ opensubmit/tests/cases.py
@@ -20,6 +20,15 @@ from opensubmit.models import Grading, GradingScheme, TestMachine
 from opensubmit.models import UserProfile
 from opensubmit.models import logger
 
+from django.test.runner import DiscoverRunner as DjangoRunner
+import os
+
+class DiscoverRunner(DjangoRunner):
+    def __init__(self, **kwargs):
+        # This seems to be the only way to convince LiveServerTestCase of another port
+        os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS']='localhost:8000'
+        super(DiscoverRunner, self).__init__(kwargs)
+
 
 class AnonStruct(object):
     def __init__(self, entries):
