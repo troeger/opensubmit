b04b0a95efa066f5ba2cad2e023119a510866d76
Peter Tröger <peter@troeger.eu>
2015-09-07 08:56:10 +0200

    Fixing OpenID login

--- opensubmit/open_id.py
+++ opensubmit/open_id.py
@@ -3,7 +3,8 @@ This is our adjustment to the OpenID provider from Python Social.
 It does nothing more than adding the provider URL configured in our settings file.
 """
 
-import social, settings
+import settings
+from social.backends.open_id import OpenIdAuth as OpenIdAuthBase
 
-class OpenIdAuth(social.backends.open_id.OpenIdAuth):
+class OpenIdAuth(OpenIdAuthBase):
 	URL = settings.OPENID_PROVIDER
