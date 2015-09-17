429a40b5d9fd2b403e9f72d62c0573b9f3797a0f
Peter Troeger <peter@troeger.eu>
2015-09-17 21:03:46 +0200

    Adding logging and error handling

--- opensubmit/social/env.py
+++ opensubmit/social/env.py
@@ -11,15 +11,17 @@ ENV_FIRST_NAME - The name of the environment variable containing the First name 
 ENV_LAST_NAME  - The name of the environment variable containing the Last name of the authenticated user.
 
 It also expects auth_url to be implemented by the derived class.
+
+Note: If you are using Apache + mod_wsgi, make sure to set 'WSGIPassAuthorization On'.
 """
 
 from social.backends.base import BaseAuth
-from social.exceptions import AuthMissingParameter
-import os
+from social.exceptions import AuthMissingParameter, AuthFailed
+import os, logging
+
+logger = logging.getLogger('OpenSubmit')
 
 class ServerEnvAuth(BaseAuth):
-    name = 'env'
-    ID_KEY = 'username'
     ENV_USERNAME = None
     ENV_EMAIL = None
     ENV_FIRST_NAME = None
@@ -32,22 +34,28 @@ class ServerEnvAuth(BaseAuth):
     def auth_complete(self, *args, **kwargs):
         """Completes loging process, must return user instance"""
         response = {}
+        if self.ENV_USERNAME not in os.environ:
+            raise AuthFailed(self.ENV_USERNAME+' variable missing on server, authentication failed.')
         uid = os.environ[self.ENV_USERNAME]
+        logger.debug("Auth complete, environment: "+str(os.environ))
         if not uid:
             # Web server did not store the authenticated user name in the environment
             raise AuthMissingParameter(self, "Missing %s in environment: %s"%(self.ENV_USERNAME, str(os.environ)))
-        response[self.ID_KEY]=uid
+        response['username']=uid
         kwargs.update({'response': response, 'backend': self})
         return self.strategy.authenticate(*args, **kwargs)
 
     def get_user_details(self, response):
         """ Complete with additional information from environment, as available. """
         result = {
-            'username': response[self.ID_KEY],
+            'username': response['username'],
             'email': os.environ[self.ENV_EMAIL],
             'first_name': os.environ[self.ENV_FIRST_NAME],
             'last_name': os.environ[self.ENV_LAST_NAME]
         }
         if result['first_name'] and result['last_name']:
             result['fullname']=result['first_name']+' '+result['last_name']
+        logger.debug("Returning user details: "+str(result))
         return result
+
+
