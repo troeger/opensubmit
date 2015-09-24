896ab76750eb4149bfc6222e1ad9e8f4999f0ba3
Peter Tröger <peter@troeger.eu>
2015-09-24 16:42:40 +0200

    Improving web application installation

--- opensubmit/wsgi.py
+++ opensubmit/wsgi.py
@@ -4,7 +4,6 @@ WSGI config for OpenSubmit project.
 import sys
 import os
 
-sys.path.append('/usr/local/opensubmit')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opensubmit.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
