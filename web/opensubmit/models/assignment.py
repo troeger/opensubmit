6a337cf7f886d50b8811084084c775132c9c4fd4
Peter Tröger <peter@troeger.eu>
2016-05-10 14:55:25 +0200

    Fix teacher backend validation script download

--- web/opensubmit/models/assignment.py
+++ web/opensubmit/models/assignment.py
@@ -77,7 +77,7 @@ class Assignment(models.Model):
             Using reverse() seems to be broken with FORCE_SCRIPT in use, so we use direct URL formulation.
         '''
         if self.pk and self.has_validity_test():
-            return settings.MAIN_URL + "download/%u/validity_testscript/secret=%s" % (self.pk, settings.JOB_EXECUTOR_SECRET)
+            return settings.MAIN_URL + "/download/%u/validity_testscript/secret=%s" % (self.pk, settings.JOB_EXECUTOR_SECRET)
         else:
             return None
 
@@ -87,7 +87,7 @@ class Assignment(models.Model):
             Using reverse() seems to be broken with FORCE_SCRIPT in use, so we use direct URL formulation.
         '''
         if self.pk and self.has_full_test():
-            return settings.MAIN_URL + "download/%u/full_testscript/secret=%s" % (self.pk, settings.JOB_EXECUTOR_SECRET)
+            return settings.MAIN_URL + "/download/%u/full_testscript/secret=%s" % (self.pk, settings.JOB_EXECUTOR_SECRET)
         else:
             return None
 
