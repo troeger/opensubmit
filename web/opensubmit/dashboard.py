668010be0beb33beb4e79eac31919a19e080f1d7
Peter Tröger <peter@troeger.eu>
2016-05-25 23:23:44 +0200

    Clearer names

--- web/opensubmit/dashboard.py
+++ web/opensubmit/dashboard.py
@@ -26,13 +26,13 @@ class TeacherDashboard(Dashboard):
         for course in courses:
             # Prepare course-related links
             links=[]
-            links.append(['Check submissions',course.grading_url(), False])
+            links.append(['Check all submissions',course.grading_url(), False])
             ass_url="%s?course__id__exact=%u"%(
                                 reverse('teacher:opensubmit_assignment_changelist'),
                                 course.pk
                             )
+            links.append(['Check assignments',ass_url, False])
             links.append(['Show current grading table',reverse('gradingtable', args=[course.pk]), False])
-            links.append(['Manage assignments, check for duplicates',ass_url, False])
             links.append(['eMail to students',reverse('mail2all', args=[course.pk]), False])
             links.append(['Edit course',reverse('teacher:opensubmit_course_change', args=[course.pk]), False])
             links.append(['Download course archive',reverse('coursearchive', args=[course.pk]), False])
