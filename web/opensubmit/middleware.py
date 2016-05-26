e695c3f1a1a276179124f72b570d9d614d404222
Peter Tröger <peter@troeger.eu>
2016-05-26 10:44:25 +0200

    Don't try to register courses for anonymous users

--- web/opensubmit/middleware.py
+++ web/opensubmit/middleware.py
@@ -7,8 +7,9 @@ class CourseRegister():
 		Sanity checks are done in the model method for adding it, which may throw a 404 exception.
 	'''
 	def process_request(self, request):
-	    if 'course' in request.GET and request.user:
+	    if 'course' in request.GET and request.user and request.user.is_authenticated():
         	course_id=request.GET['course']
+
         	request.user.profile.add_course_safe(course_id)
         	logger.debug("Adding user %u to course %s"%(request.user.pk, course_id))
 		return None
