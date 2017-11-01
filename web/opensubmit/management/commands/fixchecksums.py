a512a2741a507b66d67f6b6b9b87a8b4d5e1de9c
Peter Tröger <peter@troeger.eu>
2017-11-01 19:37:29 +0100

    Fix indentations

--- web/opensubmit/management/commands/fixchecksums.py
+++ web/opensubmit/management/commands/fixchecksums.py
@@ -5,12 +5,12 @@ class Command(BaseCommand):
     help = 'Refreshes all MD5 checksums for uploaded files'
     def handle(self, *args, **options):
         print("Scanning existing file uploads ...")
-    	files = SubmissionFile.valid_ones.all()
-    	for f in files:
-    		print("Updating checksum for %s ..."%(str(f)))
-    		try:
-	    		f.md5 = f.attachment_md5()
-	    		f.save()
-	    	except IOError as e:
-	    		print("Failed: "+str(e))
+        files = SubmissionFile.valid_ones.all()
+        for f in files:
+            print("Updating checksum for %s ..."%(str(f)))
+            try:
+                f.md5 = f.attachment_md5()
+                f.save()
+            except IOError as e:
+                print("Failed: "+str(e))
 
