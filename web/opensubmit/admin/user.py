8e7b117b5dea9ee992f560ba1e4a3cc2d7a4ed6d
Peter Tröger <peter@troeger.eu>
2017-11-03 01:46:10 +0100

    Support role assignment in user list, closes #9.

--- web/opensubmit/admin/user.py
+++ web/opensubmit/admin/user.py
@@ -1,11 +1,14 @@
 # User admin interface
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from opensubmit.models import UserProfile
 from django.shortcuts import render
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+
+from opensubmit.models import UserProfile
+from opensubmit.security import make_student, make_tutor, make_owner, make_admin, STUDENT_TUTORS_GROUP_NAME, COURSE_OWNERS_GROUP_NAME
+
 
 class UserProfileInline(admin.TabularInline):
     model = UserProfile
@@ -16,6 +19,29 @@ class UserProfileInline(admin.TabularInline):
 
 def study_program(user):
     return user.profile.study_program
+
+def is_student(user):
+    return user.is_staff
+is_student.short_description = "Backend?"
+is_student.boolean = True
+
+def is_tutor(user):
+    return user.groups.filter(name=STUDENT_TUTORS_GROUP_NAME).exists()
+is_tutor.short_description = "Tutor?"
+is_tutor.boolean = True
+
+def is_owner(user):
+    return user.groups.filter(name=COURSE_OWNERS_GROUP_NAME).exists()
+is_owner.short_description = "Owner?"
+is_owner.boolean = True
+
+def is_admin(user):
+    return user.is_superuser
+is_admin.short_description = "Admin?"
+is_admin.boolean = True
+
+def groups(user):
+    return ",".join(str(group) for group in user.groups.all())
 
 def student_id(user):
     return user.profile.student_id
@@ -29,7 +55,7 @@ def social_login_id(user):
 social_login_id.short_description = "Social Login ID"
 
 class UserAdmin(DjangoUserAdmin):
-    actions = ['mergeusers',]
+    actions = ['mergeusers','make_student','make_tutor','make_owner','make_admin']
     fieldsets = (
         ("User Data", {
             "classes": ("grp-collapse grp-open",),
@@ -53,7 +79,27 @@ class UserAdmin(DjangoUserAdmin):
         css = {'all': ('css/teacher.css',)}
 
     inlines = (UserProfileInline, )
-    list_display = ('username', 'email', 'first_name', 'last_name', student_id, study_program, 'is_staff',  social_login_id)
+    list_display = ('username', 'email', 'first_name', 'last_name', student_id, study_program, is_student, is_tutor, is_owner, is_admin, social_login_id)
+
+    def make_student(self, request, queryset):
+        for user in queryset:
+            make_student(user)
+    make_student.short_description = "Configure selected users as student"
+
+    def make_tutor(self, request, queryset):
+        for user in queryset:
+            make_tutor(user)
+    make_tutor.short_description = "Configure selected users as tutor"
+
+    def make_owner(self, request, queryset):
+        for user in queryset:
+            make_owner(user)
+    make_owner.short_description = "Configure selected users as course owner"
+
+    def make_admin(self, request, queryset):
+        for user in queryset:
+            make_admin(user)
+    make_admin.short_description = "Configure selected users as admin"
 
     def mergeusers(modeladmin, request, queryset):
         if len(queryset) != 2:
