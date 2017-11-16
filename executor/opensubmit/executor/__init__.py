35775ebd0440de2d8ba35315246e8350404724ce
Peter Tröger <peter@troeger.eu>
2017-11-16 23:54:06 +0100

    Update docs

--- executor/opensubmit/executor/__init__.py
+++ executor/opensubmit/executor/__init__.py
@@ -4,13 +4,12 @@
     This library has two parts: The validator support functions and the
     daemon functionality for fetching and running validator scripts.
 
-    Daemon: python3 -m opensubmit.executor.cmdline
-    Library: Check Submission class.
+    You should add a cron job for the following call:
+       python3 -m opensubmit.executor.cmdline
+
+    For writing test scripts, check the manual at open-submit.org
 '''
 
+# The default location of the config file for the executor
 CONFIG_FILE_DEFAULT='/etc/opensubmit/executor.ini'
-
-
-
-
 
