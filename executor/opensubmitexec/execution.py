f979b02fe417efea0e8fa7fd5bc8914f23df17f8
Peter Tröger <peter@troeger.eu>
2017-11-27 13:02:18 +0100

    PEP-8

--- executor/opensubmitexec/execution.py
+++ executor/opensubmitexec/execution.py
@@ -2,59 +2,69 @@
     Functions related to command execution on the local host.
 '''
 
-from .result import Result, PassResult, FailResult
 
+import os
+import sys
+import platform
+import subprocess
+import time
+
+from .result import FailResult, PassResult
 
 import logging
 logger = logging.getLogger('opensubmitexec')
 
-import os, sys, platform, subprocess, signal
-from threading import Timer
 
 def kill_longrunning(config):
     '''
-        Terminate everything under the current user account that has run too long.
-        This is a final safeguard if the subprocess timeout stuff is not working.
-        You better have no production servers running also under the current user account ...
+        Terminate everything under the current user account
+        that has run too long. This is a final safeguard if
+        the subprocess timeout stuff is not working.
+        You better have no production servers running also
+        under the current user account ...
     '''
-    import psutil 
+    import psutil
     ourpid = os.getpid()
     username = psutil.Process(ourpid).username
-    # check for other processes running under this account
-    timeout = config.getint("Execution","timeout")
+    # Check for other processes running under this account
+    # Take the timeout definition from the config file
+    timeout = config.getint("Execution", "timeout")
     for proc in psutil.process_iter():
         if proc.username == username and proc.pid != ourpid:
             runtime = time.time() - proc.create_time
-            logger.debug("This user already runs %u for %u seconds." % (proc.pid, runtime))
+            logger.debug("This user already runs %u for %u seconds." %
+                         (proc.pid, runtime))
             if runtime > timeout:
                 logger.debug("Killing %u due to exceeded runtime." % proc.pid)
                 try:
                     proc.kill()
-                except Exception as e:
+                except Exception:
                     logger.error("ERROR killing process %d." % proc.pid)
 
 
-def shell_execution(cmdline, working_dir, timeout=999999):
+def shell_execution(cmdline, working_dir, timeout=None):
     '''
-    Run given shell command in the given working directory with the given timeout.
-    Return according result object.
+    Run given shell command in the given working directory with the
+    given timeout. Return according result object.
+
+    cmdline is an array.
     '''
     got_timeout = False
     # Allow code to load its own libraries
-    os.environ["LD_LIBRARY_PATH"]=working_dir
+    os.environ["LD_LIBRARY_PATH"] = working_dir
     try:
         if platform.system() == "Windows":
-            proc = subprocess.Popen(cmdline, 
+            proc = subprocess.Popen(cmdline,
                                     cwd=working_dir,
-                                    stdout=subprocess.PIPE, 
-                                    stderr=subprocess.STDOUT, 
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT,
                                     creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
                                     universal_newlines=True)
         else:
-            proc = subprocess.Popen(cmdline, 
-                                    cwd=working_dir, 
-                                    stdout=subprocess.PIPE, 
-                                    stderr=subprocess.STDOUT, 
+            proc = subprocess.Popen(cmdline,
+                                    cwd=working_dir,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT,
                                     preexec_fn=os.setsid,
                                     universal_newlines=True)
         output = None
@@ -65,23 +75,25 @@ def shell_execution(cmdline, working_dir, timeout=999999):
         except subprocess.TimeoutExpired as e:
             got_timeout = True
             logger.debug("Process killed by timeout: " + str(e))
-        
-        if output == None:
+
+        if output is None:
             output = ""
-            
+
     except Exception:
         details = str(sys.exc_info())
         logger.info("Exception on process execution: " + details)
-        return FailResult("Internal error on execution: "+details)
+        return FailResult("Internal error on execution: " + details)
 
-    logger.info("Executed {0} with error code {1}.".format(cmdline, proc.returncode))
-    if proc.returncode!=0:
-        logger.debug("Output of the failed execution:\n"+output)
+    logger.info("Executed {0} with error code {1}.".format(
+        cmdline, proc.returncode))
+    if proc.returncode != 0:
+        logger.debug("Output of the failed execution:\n" + output)
     dircontent = os.listdir(working_dir)
     logger.debug("Working directory after execution: " + str(dircontent))
 
     if got_timeout:
-        res=FailResult("Execution was terminated because it took too long (%u seconds). Output so far:\n\n%s"%(timeout,output))
+        res = FailResult(
+            "Execution was terminated because it took too long (%u seconds). Output so far:\n\n%s" % (timeout, output))
     else:
         text = 'Execution of "{0}" ended with error code {1}.\n{2}\nDirectory content as I see it:\n{3}'.format(
                ' '.join(cmdline),
@@ -92,5 +104,5 @@ def shell_execution(cmdline, working_dir, timeout=999999):
             res = PassResult(text)
         else:
             res = FailResult(text)
-            res.error_code=proc.returncode
+            res.error_code = proc.returncode
     return res
