3a5c4e70100c77fdfbffef9acd6670129fb25df3
Peter Tröger <peter@troeger.eu>
2017-11-30 01:05:10 +0100

    Stop writing C code. Idiot.

--- web/opensubmit/tests/submfiles/validation/1000fff/helloworld.c
+++ web/opensubmit/tests/submfiles/validation/1000fff/helloworld.c
@@ -3,7 +3,7 @@
 int main(int argc, char** argv) {
 	char input[20];
 	printf("Please provide your input: ");
-	scanf("%s", input);
+	fgets(input, 20, stdin);
 	printf("Your input was: %s", input);
 	return 0;
 }
