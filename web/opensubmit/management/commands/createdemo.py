aeadc142abd8d1bcef33ed202398355facf68677
Peter Tröger <peter@troeger.eu>
2018-01-22 12:10:50 +0100

    Show tutor info from validation, closes #213

--- web/opensubmit/management/commands/createdemo.py
+++ web/opensubmit/management/commands/createdemo.py
@@ -1,37 +1,59 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from opensubmit.models import Course, Assignment, UserProfile, Grading, GradingScheme, Submission, SubmissionFile
+from opensubmit.models import Course, Assignment, Grading, GradingScheme, Submission, SubmissionFile, SubmissionTestResult, TestMachine
 from opensubmit import settings
 from django.utils import timezone
 from django.core.files import File as DjangoFile
 from tempfile import NamedTemporaryFile
-import datetime, os
+import datetime
+
 
 def createSubmissionFile():
     with NamedTemporaryFile(mode="wt", delete=False, prefix=settings.MEDIA_ROOT) as tmpfile:
+        # Submission file
         tmpfile.write("The quick brown fox jumps over the lazy dog.")
         tmpfile.close()
         sf = SubmissionFile(attachment=DjangoFile(tmpfile.name))
         sf.save()
-        os.remove(tmpfile.name)
+        # os.remove(tmpfile.name)
+        # test machine
+        machine = TestMachine()
+        machine.name = "Demo Test Machine"
+        machine.save()
+        # Test results
+        val_result = SubmissionTestResult()
+        val_result.submission_file = sf
+        val_result.kind = SubmissionTestResult.VALIDITY_TEST
+        val_result.result = "Validation test result for student"
+        val_result.result_tutor = "Validation test result for tutor"
+        val_result.machine = machine
+        val_result.save()
+        full_result = SubmissionTestResult()
+        full_result.submission_file = sf
+        full_result.kind = SubmissionTestResult.FULL_TEST
+        full_result.result_tutor = "Full test result for tutor"
+        full_result.machine = machine
+        full_result.save()
         return sf
+
 
 class Command(BaseCommand):
     help = 'Creates demo data in the installation'
+
     def handle(self, *args, **options):
 
         print("Adding demo data ...")
 
         # create demo users
-        users={}
+        users = {}
         for name in ['demo_student', 'demo_cheater', 'demo_tutor', 'demo_owner']:
-            user=User.objects.filter(username=name).delete()
-            user = User.objects.create_user( username=name,
-                                             email='demo@example.org',
-                                             password=name,
-                                             first_name=name,
-                                             last_name=name)
-            users[name]=user
+            user = User.objects.filter(username=name).delete()
+            user = User.objects.create_user(username=name,
+                                            email='demo@example.org',
+                                            password=name,
+                                            first_name=name,
+                                            last_name=name)
+            users[name] = user
 
         # create demo grading
         Grading.objects.filter(title='passed (demo)').delete()
@@ -40,8 +62,10 @@ class Command(BaseCommand):
         Grading.objects.filter(title='failed (demo)').delete()
         failGrade = Grading(title='failed (demo)', means_passed=False)
         failGrade.save()
-        GradingScheme.objects.filter(title='Pass/Fail Grading Scheme (Demo)').delete()
-        passFailGrading = GradingScheme(title='Pass/Fail Grading Scheme (Demo)')
+        GradingScheme.objects.filter(
+            title='Pass/Fail Grading Scheme (Demo)').delete()
+        passFailGrading = GradingScheme(
+            title='Pass/Fail Grading Scheme (Demo)')
         passFailGrading.save()
         passFailGrading.gradings.add(passGrade)
         passFailGrading.gradings.add(failGrade)
@@ -107,7 +131,6 @@ class Command(BaseCommand):
         )
         ass3.save()
 
-
         # create demo assignment without deadlines
         ass4 = Assignment(
             title='Demo A4: Graded group work with deadline being over',
@@ -139,5 +162,3 @@ class Command(BaseCommand):
             state=Submission.SUBMITTED_TESTED,
             file_upload=createSubmissionFile()
         ).save()
-
-
