609735919c9c8cbac8285c0a57eff7d4480d2474
Peter Tröger <peter@troeger.eu>
2018-02-13 22:33:52 +0100

    ARG vs. ENV

--- web/docker/docker-entry.sh
+++ web/docker/docker-entry.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 set -e
 
+# Docker image startup script
+#
+# Expects the following environment variables:
+#
+# OPENSUBMIT_SERVER_HOST: URL of the server installation
+# OPENSUBMIT_SERVER_MEDIAROOT: Local path to media storage
+# OPENSUBMIT_DATABASE_NAME: PostgreSQL database name
+# OPENSUBMIT_DATABASE_USER: PostgreSQL database user
+# OPENSUBMIT_DATABASE_PASSWORD: PostgreSQL database password
+# OPENSUBMIT_DATABASE_HOST: PostgreSQL database host
+
+# (Re-)create OpenSubmit configuration from env variables
+# Create OpenSubmit and Apache configuration file
+opensubmit-web configcreate --server-host=$OPENSUBMIT_SERVER_HOST \
+                            --server-mediaroot=$OPENSUBMIT_SERVER_MEDIAROOT \
+                            --database-name=$OPENSUBMIT_DATABASE_NAME \
+                            --database-user=$OPENSUBMIT_DATABASE_USER \
+                            --database-password=$OPENSUBMIT_DATABASE_PASSWORD \
+                            --database-host=$OPENSUBMIT_DATABASE_HOST \
+                            --database-engine=postgresql
+opensubmit-web apachecreate
+
 # Wait for postgres to come up
-while ! nc -z db 5432 2>/dev/null
+while ! nc -z $OPENSUBMIT_DATABASE_HOST 5432 2>/dev/null
 do
     let elapsed=elapsed+1
     if [ "$elapsed" -gt 90 ] 
