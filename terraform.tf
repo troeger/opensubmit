5c35b883970791be39f636b2b5d94bcdf55ddf5e
Peter Tröger <peter@troeger.eu>
2018-02-16 00:05:36 +0100

    Switch to docker compose on cloud resource

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,12 @@
 variable "region" {default = "europe-west3"}
 
 # The Google Cloud Engine account name
-variable "account" {default = "ptr_troeger"}
+variable "account" {default = "root"}
+
+# A mount point without "noexec".
+# Mainly a problem with the COS images.
+variable "script_storage" {default = "/var/lib/docker"}
+
 
 provider "google" {
   project     = "opensubmit"
@@ -11,58 +16,35 @@ provider "google" {
   credentials = "${file("google_account.json")}"
 }
 
-# Create database instance
-resource "google_sql_database_instance" "opensubmit" {
-  region      = "${var.region}"
-  database_version = "POSTGRES_9_6"
-
-  settings {
-    tier = "db-f1-micro"
-  }
-}
-
-# Create database
-resource "google_sql_database" "opensubmit" {
-  name      = "opensubmit"
-  instance  = "${google_sql_database_instance.opensubmit.name}"
-}
-resource "google_sql_user" "users" {
-  name     = "opensubmit"
-  instance = "${google_sql_database_instance.opensubmit.name}"
-  host     = "%"
-  password = "opensubmit"
-}
-
-
 # Generate SSH key for further access
 resource "tls_private_key" "vmkey" {
     algorithm = "RSA"
     rsa_bits = 4096
 }
 
-# Create firewall rule for inbound traffic
-resource "google_compute_firewall" "default" {
-  name    = "opensubmit"
-  network = "default"
+# Generate static address for web server
+resource "google_compute_address" "opensubmit" {
+  name = "opensubmit"
+}
 
-  allow {
-    protocol = "icmp"
-  }
+# Prepare tailored docker-compose file
+data "template_file" "docker-compose-yml" {
+  template = "${file("${path.module}/docker-compose-terraform.yml")}"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
+  vars {
+    external_ip = "${google_compute_address.opensubmit.address}"
   }
 }
 
-# Create virtual machine with CoreOS
-resource "google_compute_instance" "opensubmit-web" {
-  name         = "opensubmit-web" 
+# Create virtual machine 
+resource "google_compute_instance" "opensubmit" {
+  name         = "opensubmit" 
   machine_type = "f1-micro" 
   zone         = "${var.region}-a" 
   allow_stopping_for_update = "true"
 
-  depends_on = ["tls_private_key.vmkey"]
+  # Automatically configure firewall rules for ingress HTTP
+  tags = ["http-server"]
 
   service_account {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
@@ -70,41 +52,55 @@ resource "google_compute_instance" "opensubmit-web" {
 
   boot_disk {
     initialize_params {
-      image = "coreos-cloud/coreos-stable"
+      image = "cos-cloud/cos-stable"
     }
   }
  
   network_interface {
     network = "default"
-
     access_config {
-      // Ephemeral IP
+         nat_ip = "${google_compute_address.opensubmit.address}"
     }
   }
 
-  # Store public key in meta-data for VM, inserted by Google provisioning into the OS
-  # Google does that for the default user name, so we need it here
+  # Configure instance metadata
   metadata {
+    # Establish our custom SSH key, so that the file provisioner can copy stuff
     sshKeys = "${var.account}:${tls_private_key.vmkey.public_key_openssh}"
+    startup-script = "${var.script_storage}/startup.sh"
   }
 
+  connection {
+    type     = "ssh"
+    user     = "${var.account}"
+    private_key = "${tls_private_key.vmkey.private_key_pem}"
+    script_path = "${var.script_storage}/terraform.sh"
+  }
+
+  provisioner "file" {
+    content     = "${data.template_file.docker-compose-yml.rendered}"
+    destination = "docker-compose.yml"
+  }
+
+  # Store the startup script in the VM
+  # In short, this runs docker-compose from a docker image to fire up
+  # our customized docker-compose.yml (see data.template_file.docker-compose-yml above)
+  # We basically run Docker to run Docker Compose which starts our Docker images in detached mode.
+  # Phew.
+  provisioner "file" {
+    content     = <<EOF
+#!/bin/sh
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/rootfs/$PWD" -w="/rootfs/$PWD" docker/compose:1.13.0 up -d
+EOF
+    destination = "${var.script_storage}/startup.sh"
+  }
+
+  # Run startup script in instance as last step
+  # This currently fails with the Google COS image, so you have to do this manually
   provisioner "remote-exec" {
     inline = [
-      "docker  pull troeger/opensubmit-web",
-      "docker  pull troeger/opensubmit-exec",
-      "export OPENSUBMIT_SERVER_HOST=http://${google_compute_instance.opensubmit-web.network_interface.0.access_config.0.assigned_nat_ip}",
-      "export OPENSUBMIT_SERVER_MEDIAROOT=/tmp/",
-      "export OPENSUBMIT_DATABASE_NAME=opensubmit",
-      "export OPENSUBMIT_DATABASE_USER=opensubmit",
-      "export OPENSUBMIT_DATABASE_PASSWORD=opensubmit",
-      "export OPENSUBMIT_DATABASE_HOST=${google_sql_database_instance.opensubmit.ip_address.0.ip_address}",
-      "docker run -d --publish=80:8000 -e OPENSUBMIT_SERVER_HOST -e OPENSUBMIT_SERVER_MEDIAROOT -e OPENSUBMIT_DATABASE_NAME -e OPENSUBMIT_DATABASE_USER -e OPENSUBMIT_DATABASE_PASSWORD -e OPENSUBMIT_DATABASE_HOST troeger/opensubmit-web",
-      "docker run -d -e OPENSUBMIT_SERVER_HOST troeger/opensubmit-exec"
+      "chmod u+x ${var.script_storage}/startup.sh",
+      "sudo google_metadata_script_runner --script-type startup"
     ]
-    connection {
-      type     = "ssh"
-      user     = "${var.account}"
-      private_key = "${tls_private_key.vmkey.private_key_pem}"
-    }
   }
 }
