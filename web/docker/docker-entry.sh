a33cb27024bdd1b78c7b52a911106335270e8e50
Peter Tröger <peter@troeger.eu>
2018-02-21 09:13:48 +0100

    Introduce ENV-based configuration

--- web/docker/docker-entry.sh
+++ web/docker/docker-entry.sh
@@ -4,22 +4,7 @@ set -e
 # Docker image startup script
 
 # (Re-)create OpenSubmit configuration from env variables
-# Create OpenSubmit and Apache configuration file
-opensubmit-web configcreate --server_host=$OPENSUBMIT_SERVER_HOST \
-                            --server_mediaroot=$OPENSUBMIT_SERVER_MEDIAROOT \
-                            --server_hostaliases=$OPENSUBMIT_SERVER_HOST_ALIASES \
-                            --database_name=$OPENSUBMIT_DATABASE_NAME \
-                            --database_user=$OPENSUBMIT_DATABASE_USER \
-                            --database_password=$OPENSUBMIT_DATABASE_PASSWORD \
-                            --database_host=$OPENSUBMIT_DATABASE_HOST \
-                            --database_engine=$OPENSUBMIT_DATABASE_ENGINE \
-                            --login_google_oauth_key=$OPENSUBMIT_LOGIN_GOOGLE_OAUTH_KEY \
-                            --login_google_oauth_secret=$OPENSUBMIT_LOGIN_GOOGLE_OAUTH_SECRET \
-                            --login_twitter_oauth_key=$OPENSUBMIT_LOGIN_TWITTER_OAUTH_KEY \
-                            --login_twitter_oauth_secret=$OPENSUBMIT_LOGIN_TWITTER_OAUTH_SECRET \
-                            --login_github_oauth_key=$OPENSUBMIT_LOGIN_GITHUB_OAUTH_KEY \
-                            --login_github_oauth_secret=$OPENSUBMIT_LOGIN_GITHUB_OAUTH_SECRET
-
+opensubmit-web configcreate 
 opensubmit-web apachecreate
 
 # Wait for postgres to come up
