c3f9660672d16d1465c3484397e4a3c94536658b
Peter Tröger <peter@troeger.eu>
2018-10-12 08:50:45 +0200

    Implement LTI views for submissions

--- web/opensubmit/views/lti.py
+++ web/opensubmit/views/lti.py
@@ -1,11 +1,13 @@
-from django.shortcuts import redirect, get_object_or_404, render_to_response
+from django.shortcuts import redirect, get_object_or_404, render
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.decorators.csrf import csrf_exempt
 from django.views import View
+from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 
 from oauthlib.oauth1 import RequestValidator
 from lti.contrib.django import DjangoToolProvider
@@ -13,7 +15,9 @@ from lti import ToolConfig
 
 from opensubmit.social import lti
 from opensubmit import settings
-from opensubmit.models import Assignment, LtiResult
+from opensubmit.models import Assignment, LtiResult, Submission
+from opensubmit.forms import getSubmissionForm
+from opensubmit.views.frontend import SubmissionNewView, SubmissionWithdrawView
 
 import sys
 import logging
@@ -30,7 +34,11 @@ class LtiRequestValidator(RequestValidator):
         OAuth validator that considers Moodle and Opal LMS specifics,
         and relates the auth session to a particular course and its
         LTI credentials
+
+        Dummy client handling as described in
+        http://oauthlib.readthedocs.io/en/latest/_modules/oauthlib/oauth1/rfc5849/request_validator.html
     '''
+    dummy_client = 'NiemandWuerdeEinenDeutschenLtiKeyAnlegenDerSoLangIstAlsoEinGuterDummy'
     client_key_length = (20, 100)   # relax default restrictions
     nonce_length = (20, 100)        # relax default restrictions
     # relax default restrictions, sometimes Moodle OAuth timestamps are too long ago
@@ -41,7 +49,10 @@ class LtiRequestValidator(RequestValidator):
         self.assignment = get_object_or_404(Assignment, pk=assignment_id)
 
     def get_client_secret(self, client_key, request):
-        return self.assignment.course.lti_secret
+        if client_key == self.dummy_client:
+            return self.dummy_client + 'MitSecret'
+        else:
+            return self.assignment.course.lti_secret
 
     @property
     def enforce_ssl(self):
@@ -81,7 +92,7 @@ def store_report_link(backend, user, response, *args, **kwargs):
 @method_decorator(xframe_options_exempt, name='dispatch')
 class DispatcherView(View):
     '''
-        View for providing OpenSubmit assignments through LTI.
+        "LMS Tool URL" view for providing OpenSubmit assignments through LTI.
     '''
 
     def get(self, request, pk):
@@ -114,14 +125,60 @@ class DispatcherView(View):
             logger.debug("Valid OAuth request through LTI")
             if request.user.is_authenticated:
                 logger.debug("LTI consumer is already authenticated")
-                return redirect(reverse('new', args=[pk]))
+                return redirect(reverse('lti_submission', args=[pk]))
             else:
                 logger.debug("LTI consumer needs OpenSubmit user, starting auth pipeline")
                 # Store data being used by the Django Social Auth Provider for account creation
                 data = request.POST.copy()
                 data['assignment_pk'] = pk
                 request.session[lti.SESSION_VAR] = data
-                return redirect(reverse('social:begin', args=['lti']) + "?next=" + reverse('new', args=[pk]))
+                return redirect(reverse('social:begin', args=['lti']) + "?next=" + reverse('lti_submission', args=[pk]))
         else:
             logger.error("Invalid OAuth request through LTI")
             raise PermissionDenied
+
+
+class WithdrawView(SubmissionWithdrawView):
+    template_name = 'lti_withdraw.html'
+
+    def get_success_url(self):
+        submission = super(UpdateView, self).get_object()
+        return reverse('lti_submission', args=[submission.assignment.pk])
+
+
+class SubmissionView(SubmissionNewView):
+    '''
+    Special version of the submission dialogue for LTI
+    integration.
+
+    The POST logic is taken from the full 'new submission' view,
+    everything else needs to be tailored for the LTI case.
+    '''
+
+    template_name = 'lti_new.html'
+    redirect_on_success = '.'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.ass = get_object_or_404(Assignment, pk=kwargs['pk'])
+        self.submission = request.user.authored.all().filter(assignment=self.ass).exclude(state=Submission.WITHDRAWN)
+
+        if self.submission:
+            self.submission = self.submission[0]
+            logger.debug("Found existing submission: " + str(self.submission))
+        else:
+            logger.debug("No submission found for this user.")
+            # Check whether new submissions are allowed.
+            if not self.ass.can_create_submission(user=request.user):
+                logger.warn("User is not allowed to create a new submission.")
+                raise PermissionDenied(
+                    "You are not allowed to create a submission for this assignment")
+            self.SubmissionForm = getSubmissionForm(self.ass)
+        # continue with dispatching to get() / post() methods
+        return super(TemplateView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        if self.submission:
+            return render(request, 'lti_details.html', {'submission': self.submission})
+        else:
+            submissionForm = self.SubmissionForm(request.user, self.ass)
+            return render(request, 'lti_new.html', {'submissionForm': submissionForm, 'assignment': self.ass})
