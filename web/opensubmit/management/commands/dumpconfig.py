d714129bb61d6a8e292a3d495fcadbd2e1d34526
Peter Tröger <peter@troeger.eu>
2018-11-12 21:57:08 +0100

    Don't dump secrets

--- web/opensubmit/management/commands/dumpconfig.py
+++ web/opensubmit/management/commands/dumpconfig.py
@@ -2,10 +2,16 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User, Group
 from opensubmit.signalhandlers import check_permission_system
 
-skiplist = ['AUTHENTICATION_BACKENDS', 'EMAIL_BACKEND', 'LOG_FILE', 'FORCE_SCRIPT_NAME', 'GRAPPELLI_ADMIN_TITLE', 'GRAPPELLI_INDEX_DASHBOARD', 'GRAPPELLI_SWITCH_USER', 'INSTALLED_APPS', 'MIDDLEWARE_CLASSES', 'NOT_CONFIGURED_VALUE', 'ROOT_URLCONF', 'SOCIAL_AUTH_PIPELINE', 'SOCIAL_AUTH_URL_NAMESPACE', 'STATICFILES_FINDERS', 'TEMPLATES', 'TEST_RUNNER']
+skiplist = ['AUTHENTICATION_BACKENDS', 'EMAIL_BACKEND', 'LOG_FILE', 
+            'FORCE_SCRIPT_NAME', 'GRAPPELLI_ADMIN_TITLE', 'GRAPPELLI_INDEX_DASHBOARD',
+            'GRAPPELLI_SWITCH_USER', 'INSTALLED_APPS', 'MIDDLEWARE_CLASSES', 
+            'NOT_CONFIGURED_VALUE', 'ROOT_URLCONF', 'SOCIAL_AUTH_PIPELINE', 
+            'SOCIAL_AUTH_URL_NAMESPACE', 'STATICFILES_FINDERS', 'TEMPLATES', 'TEST_RUNNER']
+
 
 class Command(BaseCommand):
     help = 'Dumps effective configuration after config file parsing.'
+
     def handle(self, *args, **options):
         import opensubmit.settings as s
         for name in dir(s):
@@ -16,4 +22,7 @@ class Command(BaseCommand):
                 value = getattr(s, name)
                 print("LOGGING: {0}".format(value['handlers']))
             elif name.isupper() and name not in skiplist:
-                print("{0}: {1}".format(name, getattr(s, name)))
+                if "SECRET" in name:
+                    print("{0}: {1}".format(name, "............." + getattr(s, name)[-3:]))
+                else:
+                    print("{0}: {1}".format(name, getattr(s, name)))
