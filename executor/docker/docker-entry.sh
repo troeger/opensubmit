815cf6273b53b8600df567276e3007822a04d82f
Peter Tröger <peter@troeger.eu>
2018-12-04 16:23:14 +0100

    Better logging

--- executor/docker/docker-entry.sh
+++ executor/docker/docker-entry.sh
@@ -10,7 +10,7 @@ set -e
 # (Re-)create OpenSubmit configuration from env variables
 opensubmit-exec configcreate $OPENSUBMIT_SERVER_HOST
 
-echo "Waiting for web server to start ..."
+echo "Waiting for web server at $OPENSUBMIT_SERVER_HOST to start ..."
 # Wait for web server to come up
 until $(curl --output /dev/null --silent --head --fail $OPENSUBMIT_SERVER_HOST); do
     echo '... still waiting ...'
