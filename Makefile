c85131dd753576ae6f8c08ce61bcd2640265fb7e
Peter Tröger <peter@troeger.eu>
2019-03-05 10:24:20 +0100

    Bump version: 0.7.19 → 0.7.20

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL = /bin/bash
-VERSION = 0.7.19
+VERSION = 0.7.20
 
 .PHONY: build docs check-venv
 
--- web/setup.py
+++ web/setup.py
@@ -27,7 +27,7 @@ data_files = static+templates+templatetags
 
 setup(
     name = 'opensubmit-web',
-    version = '0.7.19',
+    version = '0.7.20',
     url = 'https://github.com/troeger/opensubmit',
     license='AGPL',
     author = 'Peter Tröger',
