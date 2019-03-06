ea2af2565e525f2bf655bbe60d06dfac61b4377a
Peter Tröger <peter@troeger.eu>
2019-03-06 09:43:30 +0100

    Extended Java example

--- examples/helloworld_java/validator.py
+++ examples/helloworld_java/validator.py
@@ -2,6 +2,11 @@ from opensubmitexec.compiler import JAVAC
 
 
 def validate(job):
+
+    # Make sure that the right file name is given
+    job.ensure_files(['HelloWorld.java'])
+
+    # Run the compiler
     job.run_compiler(compiler=JAVAC, inputs=['HelloWorld.java'])
 
     # Run the compilation result.
