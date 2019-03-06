98091c888cf2024b4423e0800bed8acda1f3878b
Peter Tröger <peter@troeger.eu>
2019-03-06 08:01:07 +0100

    OPENSUBMIT_SERVER_HOST becomes OPENSUBMIT_SERVER_URL on executor, ease Kuberneters deploy

--- executor/docker/docker-entry.sh
+++ executor/docker/docker-entry.sh
@@ -5,14 +5,14 @@ set -e
 #
 # Expects the following environment variables:
 #
-# OPENSUBMIT_SERVER_HOST: URL of the server installation
+# OPENSUBMIT_SERVER_URL: URL of the server installation
 
 # (Re-)create OpenSubmit configuration from env variables
-opensubmit-exec configcreate $OPENSUBMIT_SERVER_HOST
+opensubmit-exec configcreate $OPENSUBMIT_SERVER_URL
 
 echo "Waiting for web server to start ..."
 # Wait for web server to come up
-until $(curl --output /dev/null --silent --head --fail $OPENSUBMIT_SERVER_HOST); do
+until $(curl --output /dev/null --silent --head --fail $OPENSUBMIT_SERVER_URL); do
     echo '... still waiting ...'
     sleep 5
 done
