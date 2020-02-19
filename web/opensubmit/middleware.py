1df991905ca07d99f7d514280a06486698083fbb
Peter Tröger <peter@troeger.eu>
2020-02-19 21:51:53 +0100

    Convert middleware to new style

--- web/opensubmit/middleware.py
+++ web/opensubmit/middleware.py
@@ -3,21 +3,22 @@ from django.contrib import messages
 import logging
 logger = logging.getLogger('OpenSubmit')
 
-class CourseRegister():
+
+def CourseRegister(get_response):
     '''
         Allows to enable a course for an user based on a GET parameter with the course ID.
         Sanity checks are done in the model method for adding it, which may throw a 404 exception.
     '''
-    def process_request(self, request):
+    def middleware(request):
         if 'course' in request.GET:
             if request.user and request.user.is_authenticated():
                 # We got a user being logged in. Register him for the course.
                 title = request.user.profile.add_course_safe(request.GET['course'])
-                messages.add_message(request, messages.INFO, "You were added to the course '%s'."%title)
+                messages.add_message(request, messages.INFO, "You were added to the course '%s'." % title)
             else:
                 # We got the wish for course registration, but the user is not logged in.
                 # Remember this as part of the session, which survives the login, and do it later.
-                request.session["course_registration_request"]=request.GET['course']
+                request.session["course_registration_request"] = request.GET['course']
         else:
             if request.user and request.user.is_authenticated():
                 # We got a user being logged in.
@@ -27,5 +28,6 @@ class CourseRegister():
                     course_id = request.session['course_registration_request']
                     del request.session['course_registration_request']
                     title = request.user.profile.add_course_safe(course_id)
-                    messages.add_message(request, messages.INFO, "You were added to the course '%s'."%title)
-        return None
+                    messages.add_message(request, messages.INFO, "You were added to the course '%s'." % title)
+        return get_response(request)
+    return middleware
